a77375eb6f25073f0844cd271118ffa2d2630fb9
Leo Varghese <l.varghese@hotmail.nl>
2014-11-17 11:27:47 +0100

    anatomy01.js
    anatomy01.js

---
+++ anatomy01.js
@@ -0,0 +1,29 @@
+<!doctype html>
+<html ng-app>
+<head>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
+</head>
+<body>
+    <form ng-controller="StartUpController">
+        Starting: <input ng-change="computeNeeded()" ng-model="funding.startingEstimate">
+        Recommendation: <p ng-bind='funding.needed'></p>
+        <button ng-click="requestFunding()">Get Funds</button>
+        <button ng-click="reset()">Reset</button>
+    </form>
+    <script>
+        function StartUpController($scope) {
+            $scope.funding = { startingEstimate: 0 };
+            $scope.computeNeeded = function () {
+                $scope.funding.needed = $scope.funding.startingEstimate * 10;
+            };
+            $scope.requestFunding = function () {
+                window.alert("More customers required!!");
+            };
+            $scope.reset = function () {
+                $scope.funding.startingEstimate = 0;
+                $scope.funding.needed = 0;
+            };
+        }
+    </script>
+</body>
+</html>
